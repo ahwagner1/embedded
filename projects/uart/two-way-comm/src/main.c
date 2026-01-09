9536d45f9f2c0a00985cdb964f94464b93750f3d
Ashton Wagner <wagner.ashton04@gmail.com>
2026-01-08 21:30:43 -0900

    started planning interrupts I will need

--- projects/uart/two-way-comm/src/main.c
+++ projects/uart/two-way-comm/src/main.c
@@ -35,6 +35,15 @@ void EXTI0_IRQHandler(void) {
 *   Setting up all the UART essentials for TX/RX
 *   Might move things into other functions to further divide
 */
+
+void clock_setup(void) {
+
+}
+
+void gpio_setup(void) {
+
+}
+
 void uart_setup(void) {
     // clocks setup
     RCC->AHB1ENR |= 0x1;
@@ -65,30 +74,53 @@ void uart_setup(void) {
         apb2_prescalar = 0x1 << (apb2_ppre2 - 3);
 
     USART1->CR1 |= (0x1 << 13);
-    USART1->CR1 |= (0x1 << 2);
+    USART1->CR1 |= (0x1 << 2); // rx enable
+    USART1->CR1 |= (0x1 << 3); // tx enable
     USART1->CR1 &= ~(0x1 << 12); // set the word length (0 == 1 start, 8 dataa, n stop)
     USART1->CR1 &= ~(0x1 << 15); // setting oversampling to 16 (0 bit)
     USART1->CR2 &= ~(0x3 << 12); // setting bits 13:12 to 00 for 1 stop bit
     
+    // interrupts that I could make use of
+    // bit 4 in CR1 = IDLEIE
+    // - when this bit is set to 1, an interrupt is generated when the IDLE = 1 in the SR
+    // - could be useful for determining when a transmission is allowed to be sent if RX-ing
+    // - IDLE in the SR is set to 1 by hardware, and cleared by the software sequence:
+    //      1. Read to SR
+    //      2. Read to DR
+    // - not sure why i would read the SR or DR if the idle line is detected?
+    // - or is this saying that when a TX comes in, I would already do a read to SR and DR
+    //
+    // bit 5 in CR1 = RXNEIE
+    // - when this bit is set to 1, an interrupt is generated when RXNE/ORE = 1 in SR
+    // - this will be useful, RXNE -> read data register not empty
+    // - this allows us to f off and do other things until a transmission comes in
+    // - then an interrupt notifies us of the incoming message, MUST USE
+    //
+    // bit 6 in CR1 = TCIE
+    // - this bit generates an interrupt when a frame transmit has been completed
+    // - and TXE is set which means the TDR register has been transferred to shift register
+    // - not sure if super useful for what I want to do
+    // - could be nice if I had a more complex transmission setup I think
+    // - like if I was transmitting from multiple channels in "parrallel"
+    // - when this generates, I could reliable decide the next channel thats allowed to TX
+    //
+    //
+    // I think for two way communication setup like 
+    // - Board1_PA9_TX -> Board2_PA10_RX
+    // - Board2_PA9_TX -> Board1_PA10_RX
+    //
+    // I would want the idle line interrupt and the RXNEIE interrupt
+    // idle line lets a board know it's allowed to transmit. useful when:
+    // - board is doing other things, but now know it can transmit if needed
+    // - board is waiting to transmit
+    // RXNEIE lets a board know that it's receiving a transmit. useful when:
+    // - board is doing other things but still needs to be aware of transmits
+    
     if (clock_source == 0 || clock_source == 1) {
         uint32_t clock_frequency = clock_source ? HSE_FREQ : HSI_FREQ;
 
-        // a little verbose, I could simplify this to just 16 * 9600 baud but thats lame
-        uint8_t over8 = (USART1->CR1 >> 15) & 0x1;
-        float usartdiv = (clock_frequency / apb2_prescalar) / (8 * (2 - over8) * 9600);
-        uint32_t mantissa = (uint32_t)(usartdiv);
-
-        uint32_t fraction_multiplier = over8 ? 8 : 16;
-        uint32_t fraction = (uint32_t)((usartdiv - mantissa) * fraction_multiplier + 0.5); // 0.5 for rounding
-        
-        // handle potential fraction overflow
-        if (fraction >= fraction_multiplier) {
-            mantissa++;
-            fraction = 0;
-        }
-        
-        USART1->BRR = (mantissa << 4) | (fraction & (over8 ? 0x7 : 0xF)); // reserved bits are all supposed to be 0
-                                                                          // so this method is fine for setting BRR
+        uint32_t periphclk = clock_frequency / apb2_prescalar;
+        USART1->BRR = ((periphclk + (9600/2)) / 9600);
     }
     else if (clock_source == 2) {
         // PLL clock, calc using formula from ref man
@@ -102,6 +134,7 @@ int main(void) {
 
     // super loop go brrrrr
     while (1) {
+        // need a way to check if the line is currently transmitting, or if it's receiving
 
     }
 
