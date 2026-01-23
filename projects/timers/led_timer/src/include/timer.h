d4947bf8a91e0db3bb71ff5c19ef0850037002c3
Ashton Wagner <125780429+ahwagner1@users.noreply.github.com>
2026-01-23 12:07:11 -0900

    fixed error in timer def

--- projects/timers/led_timer/src/include/timer.h
+++ projects/timers/led_timer/src/include/timer.h
@@ -66,7 +66,7 @@ typedef struct {
     volatile uint32_t RES2;
     volatile uint32_t CCR1;
     volatile uint32_t CCR2;
-    volatile uint32_t RES3;
+    volatile uint32_t RES3[4]; // 0x3c to 0x4c is reserved
 } TIM9_Def;
 
 typedef struct {
@@ -83,17 +83,20 @@ typedef struct {
     volatile uint32_t ARR;
     volatile uint32_t RES2;
     volatile uint32_t CCR1;
-    volatile uint32_t RES3;
+    volatile uint32_t RES3[5]; // 0x38 to 0x4c is reserved
     volatile uint32_t OR;
 } TIM10_11_Def;
 
-#define TIM1 ((TIM1_Def*)0x40010000)
+// on apb1
 #define TIM2 ((TIM2_5_Def*)0x40000000)
 #define TIM3 ((TIM2_5_Def*)0x40000400)
 #define TIM4 ((TIM2_5_Def*)0x40000800)
 #define TIM5 ((TIM2_5_Def*)0x40000C00)
+
+// on apb2
+#define TIM1 ((TIM1_Def*)0x40010000)
 #define TIM9 ((TIM9_Def*)0x40014000)
 #define TIM10 ((TIM10_11_Def*)0x40014400)
-#define TIM10 ((TIM10_11_Def*)0x40014800)
+#define TIM11 ((TIM10_11_Def*)0x40014800)
 
 #endif /* TIMER_H */
